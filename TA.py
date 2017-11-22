f079b565bf5b7d0308fea54b515dea714be1162b
Jurikay <jurek.baumann@gmail.com>
2017-11-22 12:25:52 +0100

    rewritten to be able to do without .csv import and export

--- TA.py
+++ TA.py
@@ -3,101 +3,118 @@
 
 # by Jurek Baumann
 
-from botLogic import *
-import datetime
+"""Convert Binance Kline data to stockstats/ pandas DataFrames and retrieve indicator data."""
+
+
+# import datetime
 import pandas as pd
 import stockstats
+from botLogic import *
 
-# Disable chained assignment warnings. Default='warn'
+# pandas – Disable chained assignment warnings. Default='warn'
 pd.options.mode.chained_assignment = None
 
-def klinesToCsv(klines, filename):
 
-    """Make a .csv file named COINPAIR+Interval.csv.
-
-    This file contains historical trading data in the proper format to be interpreted by stockstats
-    """
-
-    # create 7 empty lists the cool way
-    date, amount, closeP, high, low, openP, volume = ([] for i in range(7))
-
-    for index in enumerate(klines):
-        i = index[0]
-
-        date.append(datetime.datetime.fromtimestamp(int(str(klines[i][6])[:-3])))
-        amount.append(klines[i][7])
-        closeP.append(klines[i][4])
-        high.append(klines[i][2])
-        low.append(klines[i][3])
-        openP.append(klines[i][1])
-        volume.append(klines[i][5])
-
-    # Make a dict from lists containing the data
-    dataTable = { 'date':date, 'amount':amount, 'close':closeP, 'high':high, 'low':low, 'open':openP, 'volume':volume }
-
-    # Build a DataFrame 'coinDataFrame' from dict dataTable
-    coinDataFrame = pd.DataFrame(dataTable, columns=['date', 'amount', 'close', 'high', 'low', 'open', 'volume'])
-
-    # Build the filename
-    file_name = str(symbol) + str(filename) + ".csv"
-
-    # Export DataFrame to csv
-    coinDataFrame.to_csv(file_name, index=False, encoding='utf-8')
-
-
-def createCSV():
-
+# entrypoint
+def getTA():
     """Make a kline API call for every time interval.
-
-    Then pass the received data to klinesToCsv.
+    Then pass the received data to createFrame.
     """
 
+    # Limit time intervals to the most interesting only
+    timeIntervals = ["1m", "5m", "15m", "30m", "1h"]
+
+    # Create a list to store API answers in
     timeFrames = []
 
-    tfIntervals = ["1m", "5m", "15m", "30m", "1h", "2h", "1d"]
+    # create a list to store dataframes in
+    allDataFrames = []
 
-    for index in enumerate(tfIntervals):
-
-        timeFrames.append([])
+    # iterate through all time intervals
+    for index in enumerate(timeIntervals):
 
         i = index[0]
 
-        timeFrames[i] = client.get_klines(symbol=val["symbol"], interval=tfIntervals[i])
+        # add an empty lists to timeFrames and allDataFrames
+        timeFrames.append([])
+        allDataFrames.append([])
 
-        klinesToCsv(timeFrames[i], tfIntervals[i])
+        # API CALL: Get kline data for each time interval
+        timeFrames[i] = client.get_klines(symbol=val["symbol"], interval=timeIntervals[i])
 
-    indicators = interpreteData(val["symbol"])
-    return indicators
-def interpreteData(symbol):
+        # store DataFrames returned from createFrame in allDataFrames
+        allDataFrames[i] = createFrame(timeFrames[i], timeIntervals[i])
 
-    """Takes current symbol and analyzes present .csv data.
+    # Once DataFrames for all time intervals are available, call interpreteData
+    indicatorData = interpreteData(timeIntervals, allDataFrames)
+
+    # Finally return indicator data
+    return indicatorData
+
+
+def createFrame(klines, filename):
+
+    """Create a pandas.DataFrame.
+
+    This object contains historical trading data in the proper format to be interpreted by stockstats.
     """
 
-    tickSize = str(val["coins"][symbol]["tickSize"])
+    # create 7 named empty lists the cool way
+    date, amount, closeP, high, low, openP, volume = ([] for i in range(7))
+
+    # iterate through API kline data
+    for index in enumerate(klines):
+
+        i = index[0]
+
+        # assign relevant data to lists
+        date.append(int(klines[i][6]))
+        amount.append(float(klines[i][7]))
+        closeP.append(float(klines[i][4]))
+        high.append(float(klines[i][2]))
+        low.append(float(klines[i][3]))
+        openP.append(float(klines[i][1]))
+        volume.append(float(klines[i][5]))
+
+    # Create a dict from lists containing the data
+    dataTable = {'date': date, 'amount': amount, 'close': closeP, 'high': high, 'low': low, 'open': openP, 'volume': volume}
+
+    # Build a DataFrame 'coinDataFrame' from the dict dataTable
+    coinDataFrame = pd.DataFrame(dataTable, columns=['date', 'amount', 'close', 'high', 'low', 'open', 'volume'])
+
+    # return the DataFrame
+    return coinDataFrame
+
+
+def interpreteData(intervals, dataFrames):
+
+    """Use stockstats to get relevant indicator data."""
+
+    # Determine number of decimal places of the considered coin
+    tickSize = str(val["coins"][val["symbol"]]["tickSize"])
     roundTo = len(str(tickSize))-2
 
+    indicatorData = dict()
 
+    # iterate through time intervals
+    for value in enumerate(intervals):
 
-    Intervals = ["1m", "5m", "15m", "30m", "1h", "2h", "1d"]
+        i = value[0]
 
-    indicators = dict()
+        # Create a StockDataFrame from the respective time interval data
+        stock = stockstats.StockDataFrame.retype(dataFrames[i])
 
-    for value in enumerate(Intervals):
-
-
-        try:
-            stock = stockstats.StockDataFrame.retype(pd.read_csv(str(symbol) + str(value[1]) + '.csv'))
-        except FileNotFoundError:
-            logging.debug("COULD NOT FIND CSV")
-
-        # Calculate chosen indicators
+        # Calculate chosen indicatorData
+        # RSI
         rsi6hData = stock['rsi_6']
         rsi12hData = stock['rsi_12']
 
+        # BOLLINGER BANDS
         medBollData = stock['boll']
         upperBollData = stock['boll_ub']
         lowerBollData = stock['boll_lb']
 
+        # TODO ADD MORE RELEVANT
         volumeDeltaData = stock['volume_delta']
 
         # get last respective entries from DataFrame
@@ -110,6 +127,9 @@ def interpreteData(symbol):
 
         volumeDelta = volumeDeltaData.iloc[-1]
 
-        indicators[value[1]] = {"rsi6h": rsi6h, "rsi12h": rsi12h, "medBoll": medBoll, "upperBoll": upperBoll, "lowerBoll": lowerBoll, "volumeDelta": volumeDelta}
-        # print(medBoll)
-    return indicators
+        # For every time interval, add the last value of the
+        # chosen indicator to a dictionary as a 'key': value pair
+        indicatorData[value[1]] = {"rsi6h": rsi6h, "rsi12h": rsi12h, "medBoll": medBoll, "upperBoll": upperBoll, "lowerBoll": lowerBoll, "volumeDelta": volumeDelta}
+
+    # return indicator data sorted by timeframe.
+    return indicatorData
